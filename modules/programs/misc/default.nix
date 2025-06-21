b259a9a2980f877bd1581de717c9123151d182c8
howl <howell.ding@outlook.com>
2025-06-21 21:18:28 +0800

    add more pkg

--- modules/programs/misc/default.nix
+++ modules/programs/misc/default.nix
@@ -1,4 +1,17 @@
 { pkgs, ... }: {
-  home-manager.sharedModules =
-    [ (_: { home.packages = with pkgs; [ zoxide fzf unzip gcc ]; }) ];
+  home-manager.sharedModules = [
+    (_: {
+      home.packages = with pkgs; [
+        gcc
+        unzip
+        zoxide
+        fzf
+        ffmpeg
+        jq
+        cmatrix
+        tty-clock
+        cbonsai
+      ];
+    })
+  ];
 }
