43bc66203bcfe4a0e2c55cfcc3885c5ec8b19e0e
howl <howell.ding@outlook.com>
2025-06-21 21:16:01 +0800

    solve suspend black screen

--- modules/hardware/gpu/nvidia/default.nix
+++ modules/hardware/gpu/nvidia/default.nix
@@ -14,7 +14,7 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
@@ -24,9 +24,9 @@
     # independent third-party "nouveau" open source driver).
     # Support is limited to the Turing and later architectures. Full list of 
     # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # https://github.com/NVIDfalseIA/open-gpu-kernel-modules#compatible-gpus 
     # Only available from driver 515.43.04+
-    open = false;
+    open = true;
 
     # Enable the Nvidia settings menu,
     # accessible via `nvidia-settings`.
