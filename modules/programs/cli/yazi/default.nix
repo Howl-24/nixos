82ba7a838b533757c9ff7d590e2b78baad891b3a
howl <howell.ding@outlook.com>
2025-06-21 22:13:56 +0800

    edit yazi config

--- modules/programs/cli/yazi/default.nix
+++ modules/programs/cli/yazi/default.nix
@@ -5,6 +5,7 @@
         enable = true;
         enableBashIntegration = true;
         enableZshIntegration = true;
+        shellWrapperName = "y";
         settings = {
           manager = {
             show_hidden = false;
@@ -23,31 +24,36 @@
             tab_size = 4;
             image_filter =
               "triangle"; # from fast to slow but high quality: nearest, triangle, catmull-rom, lanczos3
-            max_width = 1920; # maybe 1000
-            max_height = 1080; # maybe 1000
-            # max_width = 1500;
-            # max_height = 1500;
+            max_width = 3840; # maybe 1000
+            max_height = 2160; # maybe 1000
             image_quality = 90;
           };
         };
         keymap = {
           manager.prepend_keymap = [
             {
-              on = [ "e" ];
-              run = "open";
+              on = "T";
+              run = "plugin toggle-pane min-preview";
             }
             {
-              on = [ "d" ];
-              run = "remove --force";
+              on = "T";
+              run = "plugin toggle-pane max-preview";
             }
           ];
         };
-        theme = {
-          manager = { border_symbol = " "; };
-          status = {
-            separator_open = "";
-            separator_close = "";
-          };
+        flavors = {
+          dark = ./flavors/catppuccin-mocha.yazi;
+          light = ./flavors/catppuccin-latte.yazi;
+        };
+        plugins = {
+          git = pkgs.git;
+          diff = pkgs.diff;
+          glow = pkgs.glow;
+          mediainfo = pkgs.mediainfo;
+          lazygit = pkgs.lazygit;
+          full-border = pkgs.full-border;
+          yatline-catppuccin = pkgs.yatline-catppuccin;
+          toggle-pane = pkgs.toggle-pane;
         };
       };
     })
