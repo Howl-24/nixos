b1129fd2b122b88dbc48fb6009be3aa18861a02d
howl <howell.ding@outlook.com>
2025-06-25 22:36:58 +0800

    more extensions

--- modules/programs/vscode/default.nix
+++ modules/programs/vscode/default.nix
@@ -2,8 +2,9 @@
   lib,
   pkgs,
   ...
-}: {
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) ["vscode"];
+}:
+{
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "vscode" ];
   home-manager.sharedModules = [
     (_: {
       programs.vscode = {
@@ -13,10 +14,17 @@
         package = pkgs.vscode;
         profiles.default = {
           extensions = with pkgs.vscode-extensions; [
+            github.copilot
+            github.copilot-chat
+            ms-vscode-remote.remote-ssh
+            ms-vscode-remote.remote-ssh-edit
+            ms-vscode.remote-explorer
+            esbenp.prettier-vscode
+            yzhang.markdown-all-in-one
+            asvetliakov.vscode-neovim
             catppuccin.catppuccin-vsc
             catppuccin.catppuccin-vsc-icons
-            # asvetliakov.vscode-neovim
-          ];  
+          ];
         };
       };
     })
