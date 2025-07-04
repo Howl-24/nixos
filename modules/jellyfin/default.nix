48b49ea5425f87c73912992191eb142baf5ad6a3
howl <howell.ding@outlook.com>
2025-07-05 00:35:33 +0800

    add proxy

--- modules/jellyfin/default.nix
+++ modules/jellyfin/default.nix
@@ -5,6 +5,14 @@
     openFirewall = true;
     user = "${opts.username}";
   };
+  systemd.services.jellyfin = {
+    serviceConfig = {
+      Environment = [
+        "http_proxy=http://127.0.0.1:9998"
+        "https_proxy=http://127.0.0.1:9998"
+      ];
+    };
+  };
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
